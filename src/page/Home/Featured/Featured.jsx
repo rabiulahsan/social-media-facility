d7d197652fb1a95bee77266994d0aba962b95f20
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-04-30 23:18:03 +0600

    sorted descendingly all data based on likes

--- src/page/Home/Featured/Featured.jsx
+++ src/page/Home/Featured/Featured.jsx
@@ -1,8 +1,12 @@
+import UseAllPosts from "../../../Hooks/UseAllPosts";
 import useAuth from "../../../Hooks/UseAuth";
 
 const Featured = () => {
   const { user } = useAuth();
+  const [posts, isLoading] = UseAllPosts();
 
+  // sorted based on likes
+  posts.sort((a, b) => b.likes - a.likes);
   return <div></div>;
 };
 
--- src/page/Home/Home/Home.jsx
+++ src/page/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Footer from "../../../Shared/Footer/Footer";
 import Navbar from "../../../Shared/Navbar/Navbar";
+import Featured from "../Featured/Featured";
 
 const Home = () => {
   return (
     <div className="bg-slate-100">
       <Navbar></Navbar>
+      <Featured></Featured>
       <Footer></Footer>
     </div>
   );
