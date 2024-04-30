2e75ce73ac1be225553d30dc5aef152f2054cf7f
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-05-01 04:21:26 +0600

    media page  added

--- src/page/Media/Media.jsx
+++ src/page/Media/Media.jsx
@@ -1,5 +1,23 @@
+import SkeletonCard from "../../Components/SkeletonCard/SkeletonCard";
+import UseAllPosts from "../../Hooks/UseAllPosts";
+import Footer from "../../Shared/Footer/Footer";
+import Navbar from "../../Shared/Navbar/Navbar";
+import PostCard from "../../Shared/PostCard/PostCard";
+
 const Media = () => {
-  return <div></div>;
+  const [posts, isLoading] = UseAllPosts();
+  return (
+    <div className="bg-slate-100">
+      <Navbar></Navbar>
+      <div className=" flex flex-col mx-[30%] mt-[5%]">
+        {isLoading && <SkeletonCard number={16}></SkeletonCard>}
+        {posts?.map((post) => (
+          <PostCard key={post?._id} post={post}></PostCard>
+        ))}
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Media;
