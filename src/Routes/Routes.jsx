90d40a486055f5a3ffd22f422937ff1dab0f6103
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-04-30 17:42:56 +0600

    fixed error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../page/Home/Home/Home";
+import ErrorPage from "../Components/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
