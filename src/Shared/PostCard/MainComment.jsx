db62b011fc2542d4ca5cc53e711836bc4984f0e5
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-05-01 17:51:41 +0600

    comment section designed completed

--- src/Shared/PostCard/MainComment.jsx
+++ src/Shared/PostCard/MainComment.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import UseSingleUser from "../../Hooks/UseSingleUser";
+import SingleComment from "./SingleComment";
 
 /* eslint-disable react/prop-types */
 const MainComment = ({ id }) => {
@@ -16,7 +17,15 @@ const MainComment = ({ id }) => {
       .catch((error) => console.error(error));
   }, [id]);
   console.log(commentData);
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div className=" flex flex-col gap-y-5 mx-[5%]">
+        {commentData?.map((comment) => (
+          <SingleComment key={comment?._id} comment={comment}></SingleComment>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MainComment;
---
+++ src/Shared/PostCard/SingleComment.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+const SingleComment = ({ comment }) => {
+  console.log(comment);
+  return (
+    <div className="bg-slate-100 text-slate-600 rounded p-4">
+      <div className="flex items-center gap-x-3">
+        <img
+          src={
+            comment?.commentatorImage
+              ? comment?.commentatorImage
+              : "/profile.png"
+          }
+          alt=""
+          className="h-[30px] w-[30px] rounded-full object-cover"
+        />
+        <p className="font-semibold text-base">{comment?.commentatorName}</p>
+      </div>
+      <div className="mt-3 px-2">
+        <p className="text-slate-500 font-semibold text-[14px]">
+          {comment?.comment}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SingleComment;
