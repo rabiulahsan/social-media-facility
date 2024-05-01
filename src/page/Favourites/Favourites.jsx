0687ef6a4a988a26a74bf86c8afbdfedd5272d44
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-05-01 23:29:06 +0600

    sorted posts of favourite page

--- src/page/Favourites/Favourites.jsx
+++ src/page/Favourites/Favourites.jsx
@@ -9,18 +9,30 @@ import PostCard from "../../Shared/PostCard/PostCard";
 const Favourites = () => {
   const [favouriteData] = UseFavourite();
   const [posts, isLoading] = UseAllPosts();
+
+  //getting the favourite posts
   const favoritePosts = posts?.filter((post) =>
     favouriteData?.some((favorite) => favorite.postId === post?._id)
   );
 
-  console.log(favoritePosts);
+  //sorting all of them
+  const sortedFavouritePosts = favoritePosts?.slice().sort((a, b) => {
+    // Convert the postedTime strings to Date objects for comparison
+    const dateA = new Date(a.postedTime);
+    const dateB = new Date(b.postedTime);
+
+    // Sort by descending order of postedTime
+    return dateB - dateA;
+  });
+
+  // console.log(favoritePosts);
   return (
     <div>
       <Navbar></Navbar>
       <SectionTitle heading="Liked Posts"></SectionTitle>
       <div className=" flex flex-col mx-[30%]">
         {isLoading && <SkeletonCard number={16}></SkeletonCard>}
-        {favoritePosts?.map((post) => (
+        {sortedFavouritePosts?.map((post) => (
           <PostCard key={post?._id} post={post}></PostCard>
         ))}
       </div>
