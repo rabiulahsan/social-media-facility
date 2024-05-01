6c670e5bfd84ca15737ec4793e24a82f258998d9
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-05-01 12:05:34 +0600

    message page created

--- src/page/Message/Message.jsx
+++ src/page/Message/Message.jsx
@@ -1,5 +1,34 @@
+import Footer from "../../Shared/Footer/Footer";
+import Navbar from "../../Shared/Navbar/Navbar";
+import FadeAnimations from "../../Animations/FadeAnimations";
+import { Player } from "@lottiefiles/react-lottie-player";
+
 const Message = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="pt-[5%] ">
+        <FadeAnimations
+          direction="down"
+          once={false}
+          delay={0.3}
+          duration={0.5}
+        >
+          <Player
+            className="h-[300px]  w-[300px]"
+            autoplay
+            loop
+            src="/empty.json"
+          ></Player>
+        </FadeAnimations>
+
+        <p className="text-2xl font-bold text-center mt-[2%]">
+          There is no message here
+        </p>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Message;
