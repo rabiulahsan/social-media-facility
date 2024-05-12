81ceb10320638ce25fcb3c7019798c69fb7affa2
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-05-12 21:45:32 +0600

    add post bug fixed

--- src/page/Home/Home/Home.jsx
+++ src/page/Home/Home/Home.jsx
@@ -1,13 +1,15 @@
+import useAuth from "../../../Hooks/UseAuth";
 import Footer from "../../../Shared/Footer/Footer";
 import Navbar from "../../../Shared/Navbar/Navbar";
 import AddPost from "../AddPost/AddPost";
 import Featured from "../Featured/Featured";
 
 const Home = () => {
+  const { user } = useAuth();
   return (
     <div className="bg-slate-100">
       <Navbar></Navbar>
-      <AddPost></AddPost>
+      {user && <AddPost></AddPost>}
       <Featured></Featured>
       <Footer></Footer>
     </div>
