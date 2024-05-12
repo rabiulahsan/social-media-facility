1bbef675a70b82ba8f956f60717b31ca6155f5e4
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-05-12 20:22:25 +0600

    message page redesign

--- src/page/Message/Message.jsx
+++ src/page/Message/Message.jsx
@@ -1,5 +1,21 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+
 const Message = () => {
-  return <div className=""></div>;
+  return (
+    <div className="bg-white rounded p-5 h-full flex justify-center items-center">
+      <div className="">
+        <Player
+          className="h-[200px] "
+          autoplay
+          loop
+          src="/message.json"
+        ></Player>
+        <p className="text-4xl font-bold text-slate-600 text-center">
+          Start a chat
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
