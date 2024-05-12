5a80a4d3c1ad6c245a9ab0fda109895379c6c9b8
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-05-12 17:35:07 +0600

    input fixed

--- src/page/Message/ChatBox.jsx
+++ src/page/Message/ChatBox.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { IoCallSharp } from "react-icons/io5";
+import { IoIosSend } from "react-icons/io";
 const ChatBox = () => {
+  const [typingMessage, setTypingMessage] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="bg-slate-200 rounded  h-full">
+    <div className="bg-slate-200 rounded  h-full flex flex-col">
       <div className="flex justify-between items-center bg-white px-5 py-2">
         <div className="flex items-center gap-x-4">
           <img src="" alt="" className="rounded-full h-[35px] w-[35px]" />
@@ -13,8 +20,28 @@ const ChatBox = () => {
           </span>
         </div>
       </div>
-      <div className=""></div>
-      <div className=""></div>
+      <div className=" my-4 px-5">
+        <p className="  bg-white rounded-sm p-3 text-slate-500 ">hellow</p>
+      </div>
+      <div className="mt-auto px-[5%] my-3 ">
+        <form
+          onSubmit={handleSubmit}
+          className="flex justify-center items-center"
+        >
+          <input
+            className="  rounded-3xl  py-2 px-5 w-[60%] text-gray-600   focus:outline-none bg-white "
+            type="text"
+            value={typingMessage}
+            onChange={(e) => setTypingMessage(e.target.value)}
+          />
+          <button
+            type="submit"
+            className=" text-2xl   font-semibold text-slate-500 ml-4 bg-white hover:bg-slate-100 hover:text-slate-600 p-2 rounded-full"
+          >
+            <IoIosSend></IoIosSend>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/page/Message/Message.jsx
+++ src/page/Message/Message.jsx
@@ -6,11 +6,11 @@ const Message = () => {
   return (
     <div className="bg-slate-100 h-screen flex flex-col">
       <Navbar></Navbar>
-      <div className=" flex w-full gap-x-6 p-[2%]   flex-[100%]">
-        <div className="w-1/4">
+      <div className=" flex w-full gap-x-6 py-[2%] px-[9%]   flex-[100%]">
+        <div className="w-[30%]">
           <Sidebar></Sidebar>
         </div>
-        <div className="w-3/4">
+        <div className="w-[70%]">
           <ChatBox></ChatBox>
         </div>
       </div>
