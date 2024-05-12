a36724908f97dbede0377651cb1bab028929761e
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-05-13 03:06:47 +0600

    displayed my chat

--- src/page/Message/Sidebar.jsx
+++ src/page/Message/Sidebar.jsx
@@ -1,58 +1,22 @@
+import { useEffect, useState } from "react";
 import SingleChat from "./SingleChat";
+import UseSingleUser from "../../Hooks/UseSingleUser";
 
 const Sidebar = () => {
-  const users = [
-    {
-      _id: 1001,
-      name: "John",
-      lastMessage: "Hello, how are you?",
-    },
-    {
-      _id: 1002,
-      name: "Alice",
-      lastMessage: "I'm good, thank you!",
-    },
-    {
-      _id: 1003,
-      name: "Bob",
-      lastMessage: "What are you up to?",
-    },
-    {
-      _id: 1004,
-      name: "Emma",
-      lastMessage: "Just finished work.",
-    },
-    {
-      _id: 1005,
-      name: "Michael",
-      lastMessage: "Let's meet tomorrow.",
-    },
-    {
-      _id: 1006,
-      name: "Sophia",
-      lastMessage: "Sure, where?",
-    },
-    {
-      _id: 1007,
-      name: "James",
-      lastMessage: "How about the park?",
-    },
-    {
-      _id: 1008,
-      name: "Olivia",
-      lastMessage: "Sounds good!",
-    },
-    {
-      _id: 1009,
-      name: "William",
-      lastMessage: "See you there!",
-    },
-    {
-      _id: 1010,
-      name: "Charlotte",
-      lastMessage: "Sure, see you tomorrow.",
-    },
-  ];
+  const [loggedUser] = UseSingleUser();
+  // console.log(loggedUser);
+  const [myChat, setMyChat] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/chats/${loggedUser?._id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMyChat(data);
+      })
+      .catch((error) => console.error(error));
+  }, [loggedUser]);
+
+  // console.log(myChat);
 
   return (
     <div className="bg-white rounded  h-full ">
@@ -60,7 +24,7 @@ const Sidebar = () => {
         All Chats
       </p>
       <div className="flex flex-col gap-y-2 overflow-y-auto h-[520px] py-2 px-4 mt-2">
-        {users?.map((user, idx) => (
+        {myChat?.map((user, idx) => (
           <SingleChat key={idx} user={user}></SingleChat>
         ))}
       </div>
--- src/page/Message/SingleChat.jsx
+++ src/page/Message/SingleChat.jsx
@@ -1,24 +1,41 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import ActiveSidebar from "../../Components/ActiveSidebar/ActiveSidebar";
+import UseSingleUser from "../../Hooks/UseSingleUser";
 const SingleChat = ({ user }) => {
+  const [loggedUser] = UseSingleUser();
+  const [chatUser, setChatUser] = useState([]);
   // console.log(user);
+  const chatUserId = user?.users.filter((user) => user !== loggedUser?._id)[0];
+  // console.log(chatUserId);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/allusers/${chatUserId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setChatUser(data);
+      })
+      .catch((error) => console.error(error));
+  }, [chatUserId]);
+  console.log(chatUser);
+
   return (
-    <ActiveSidebar to={`/message/${user?._id}`}>
+    <ActiveSidebar to={`/message/${chatUser?._id}`}>
       <div className="rounded p-3 flex items-center gap-x-4 hover:bg-slate-200 cursor-pointer">
         <div className="">
           <img
-            src=""
+            src={chatUser?.image}
             alt=""
-            className="w-[40px] h-[40px] rounded-full object-cover"
+            className="w-[40px] h-[35px] rounded-full object-cover"
           />
         </div>
         <div className=" w-full">
           <div className="flex justify-between items-center">
-            <p className="font-bold  text-slate-600">{user?.name}</p>
+            <p className="font-bold  text-slate-600">{chatUser?.name}</p>
             <p className="text-sm text-slate-600">Time</p>
           </div>
           <div className="">
-            <p className="text-sm text-slate-600 ">{user?.lastMessage}</p>
+            <p className="text-sm text-slate-600 ">last message</p>
           </div>
         </div>
       </div>
