a8f89dbb73607ea7261f9ab362d0c078157a7bf1
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-05-13 12:10:20 +0600

    designed search field for input

--- src/page/Message/Sidebar.jsx
+++ src/page/Message/Sidebar.jsx
@@ -24,62 +24,35 @@ const Sidebar = () => {
   const handleSearch = (e) => {
     e.preventDefault();
 
-    // Get the current time and format it with AM/PM indication
-    const time = dayjs();
-    // const formatttimeedTime = dayjs().format("h:mm A");
-
-    // console.log(formattedTime);
-
-    const messageBody = {
-      senderId: loggedUser?._id,
-      text: typingMessage,
-      time: time,
-      chatId: chat?._id,
-    };
-    // console.log(messageBody);
-
     //   posting it to database
-    fetch("http://localhost:5000/messages", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(messageBody),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.insertedId) {
-          setTypingMessage("");
-          setMessages([...messages, messageBody]);
-          console.log(messageBody);
-        }
-      });
-
-    // socket.emit("send", typingMessage);
+    // fetch(`http://localhost:5000/chatusers?value=${searchValue}`)
+    //   .then((res) => res.json())
+    //   .then((data) => {});
   };
 
   return (
     <div className="bg-white rounded  h-full ">
       {/* form for searching user  */}
-      <div className="">
+      <div className=" px-12 pt-5 pb-4 ">
         <form
           onSubmit={handleSearch}
-          className="flex justify-center items-center"
+          className="flex justify-center items-center relative"
         >
           <input
-            className="rounded-3xl py-2 px-5 w-[100%] text-gray-600 focus:outline-none bg-white"
+            className="rounded-3xl py-2 px-5 w-[100%] text-gray-600 focus:outline-none bg-slate-200"
             type="text"
             placeholder="Search or start a new chat"
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
           />
+          <IoSearch className="absolute top-1/2 right-8 text-slate-400 text-lg transform -translate-y-1/2 " />
         </form>
       </div>
 
-      <p className="font-bold text-slate-600 text-xl text-center p-4 border border-b-slate-400">
+      <p className="font-bold text-slate-600 text-xl text-center p-4 border-b border-b-slate-200">
         All Chats
       </p>
-      <div className="flex flex-col gap-y-2 overflow-y-auto h-[520px] py-2 px-4 mt-2">
+      <div className="flex flex-col gap-y-2 overflow-y-auto h-[520px] py-4 px-5 mt-2">
         {myChat?.map((user, idx) => (
           <SingleChat key={idx} user={user}></SingleChat>
         ))}
